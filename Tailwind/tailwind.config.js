99d77baa53eddc9a3e0c3a86e930a6e2dad45ff4
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2024-06-16 18:48:25 +0530

    Added one more section

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["dist/*.html"],
+  content: ["dist/*.html", "dist/TrelloClone/*.html"],
   theme: {
     extend: {},
   },
