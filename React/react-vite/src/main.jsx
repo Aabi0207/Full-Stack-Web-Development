d0ca730dd4f9e016d8b3a2f072370efbb7943acf
Abhishek Biradar <abhishekbiradar0207@gmail.com>
2024-06-18 15:51:35 +0530

    Made my custom react app

--- React/react-vite/src/main.jsx
+++ React/react-vite/src/main.jsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function MyApp() {
+  return (
+    <div>
+      Custom my App
+    </div>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>visit Google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {
+    href: "https://google.com",
+    target: "_blank"
+  },
+  "Click me to visit Google"
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    // anotherElement
+    // reactElement
+  // </React.StrictMode>,
 )
